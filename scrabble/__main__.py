1e124a110f8a71d57fe09774949eaf877eb50ce6
JnyJny <erik.oshaughnessy@gmail.com>
2020-09-20 20:17:37 -0500

    initial commit

---
+++ scrabble/__main__.py
@@ -0,0 +1,15 @@
+"""
+"""
+
+from .game import Game
+
+import typer
+
+cli = typer.Typer()
+
+
+@cli.command()
+def play_scrabble():
+    """"""
+
+    pass
---
+++ scrabble/game.py
@@ -0,0 +1,6 @@
+"""
+"""
+
+
+class Game:
+    """A scrabble game"""
